a8996ba3205f899d5019d6a24633befada3bb5ab
Matilda Stoetzer <stoetzer@kth.se>
2025-05-06 20:11:21 +0200

    .

--- heat_tests.py
+++ heat_tests.py
@@ -1,1 +1,12 @@
 # tester
+
+import unittest
+import numpy as np
+
+class Tests(unittest.TestCase):
+    def test(self):
+        v = np.array([1, 1])
+        w = np.array([1, 1])
+        self.assertEqual(v, w)
+
+unittest.main()
