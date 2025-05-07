b544f4101a520b455030947b29fee82f157e7bd9
Matilda Stoetzer <stoetzer@kth.se>
2025-05-07 12:12:52 +0200

    ..

--- heat_test.py
+++ heat_test.py
@@ -1,19 +1,6 @@
 # tester för main.py
+from heat import * # behöver ej importera numpy
 import pytest
-import numpy as np
-from heat import HeatObj
 
-f = lambda x: np.sin(2*np.pi*x)**2
-l = lambda t: 0*t
-r = lambda t: 0*t
-x_int = np.array([0, 1])
-t_int = np.array([0, 0.5])
-test_object = HeatObj(f, l, r, 10, 100, x_int, t_int, D=1)
-
-#print(test_object.create_solution_matrix().shape)
-print(test_object.n)
-test_object._update_step_size()
-print(test_object.h)
-print(test_object.k)
 
 
