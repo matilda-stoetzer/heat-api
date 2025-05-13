ae4174d13fa4a84616275760d6718d9eb49bf034
Matilda Stoetzer <stoetzer@kth.se>
2025-05-13 10:53:13 +0200

    La till fixtures

--- heat_test.py
+++ heat_test.py
@@ -2,132 +2,106 @@
 from heat import *
 import pytest
 
+@pytest.fixture(scope='class')
+def example_equation():
+    return HeatEquation(
+        D=1,
+        f=lambda x: 2*x,
+        l=lambda t: 3*t,
+        r=lambda t: 3*t,
+        x_int=[0, 1],
+        t_int=[0, 1]
+    )
 
-class TestHeatEq:
-    def test_attributes(self):
+@pytest.fixture(scope='class')
+def example_solver(example_equation):
+    return ForwardDiff(
+        equation=example_equation,
+        M=10,
+        N=100
+    )
+
+
+class TestHe:
+    def test_attributes(self, example_equation):
         """Tests that the attributes is the right data type."""
-        D = 1
-        f = lambda x: 2*x
-        l = lambda t: 3*t
-        r = l
-        x = [0, 1]
-        t = [0, 1]
-        test_eq = HeatEquation(D, f, l, r, x, t)
-
-        assert type(test_eq.f) == type(f)
-        assert type(test_eq.l) == type(l)
-
-    def test_create_init_cond_array(self):
-        """Tests that the value passed to the function is the same as if it were passed to the initial condition function itself."""
-        D = 1
-        f = lambda x: 2*x
-        l = lambda t: 3*t
-        r = l
-        x = [0, 1]
-        t = [0, 1]
-        test_eq = HeatEquation(D, f, l, r, x, t)
-        z = [0, 3]
-
-        assert test_eq.create_init_cond_array(z) == f(z)
-
-    def test_create_bound_cond_array(self):
-        """Tests that the value passed to the function is the same as if it were passed to the boundary condition function itself."""
-        D = 1
-        f = lambda x: 2*x
-        l = lambda t: 3*t
-        r = l
-        x = [0, 1]
-        t = [0, 1]
-        test_eq = HeatEquation(D, f, l, r, x, t)
-        z = [0, 3]
         
-        assert test_eq.create_bound_cond_array(l, z) == l(z)
-        assert test_eq.create_bound_cond_array(r, z) == r(z)
+        assert callable(example_equation.l)
+        assert callable(example_equation.f)
+
+    def test_create_init_cond_array(self, example_equation):
+        """Tests that the value passed to the function is the same 
+        as if it were passed to the initial condition function itself."""
+        
+        z = [0, 3]
+        assert example_equation.create_init_cond_array(z) == example_equation.f(z)
+
+    def test_create_bound_cond_array(self, example_equation):
+        """Tests that the value passed to the function is the same as if it were passed to the 
+        boundary condition function itself."""
+        
+        z = [0, 3]
+        assert example_equation.create_bound_cond_array(example_equation.l, z) == example_equation.l(z)
+        assert example_equation.create_bound_cond_array(example_equation.r, z) == example_equation.r(z)
+
 
 
 class TestForwDiff:
-    def test_dimension_sol_matrix(self):
+    def test_dimension_sol_matrix(self, example_solver):
         """Tests that the dimension of the solution matrix is (M-1) x (N+1)."""
-        test_eq = HeatEquation(1, lambda x: 2*x, lambda t: 0*t, lambda t: 0*t, [0, 1], [0, 1])
-        M = 10
-        N = 100
-        test_numeric = ForwardDiff(test_eq, M, N)
-        solution_matrix = test_numeric.create_solution_matrix()
+        
+        solution_matrix = example_solver.create_solution_matrix()
+        assert solution_matrix.shape == (example_solver.m, example_solver.n+1)
 
-        assert solution_matrix.shape == (M-1, N+1)
-
-    def test_create_sol_matrix_empty(self):
+    def test_create_sol_matrix_empty(self, example_solver):
         """Tests that the solution matrix only consists of zeros when first created."""
-        test_eq = HeatEquation(1, lambda x: 2*x, lambda t: 0*t, lambda t: 0*t, [0, 1], [0, 1])
-        M = 10
-        N = 100
-        test_numeric = ForwardDiff(test_eq, M, N)
-        sol_matrix = test_numeric.create_solution_matrix()
-
+        
+        sol_matrix = example_solver.create_solution_matrix()
         np.testing.assert_array_equal(sol_matrix, np.zeros((9, 101)))
 
 
-    def test_add_initial_condition(self):
+    def test_add_initial_condition(self, example_solver):
         """Tests that the initial conditions is added in the right column."""
-        test_eq = HeatEquation(1, lambda x: 2*x, lambda t: 0*t, lambda t: 0*t, [0, 1], [0, 1])
-        M = 10
-        N = 100
-        test_numeric = ForwardDiff(test_eq, M, N)
-        w = test_numeric.create_solution_matrix()
-        init_cond_vector = test_numeric._initial_condition()
 
-        w = test_numeric.add_initial_condition(w)
+        w = example_solver.create_solution_matrix()
+        init_cond_vector = example_solver._initial_condition()
+        w = example_solver.add_initial_condition(w)
 
         np.testing.assert_array_equal(w[:,0], init_cond_vector)
 
 
-    def test_add_boundary_conditions(self):
+    def test_add_boundary_conditions(self, example_solver):
         """Tests that the boundary conditions function updates the dimension of the solution matrix 
         and adds the correct values for the boundary condition."""
-        test_eq = HeatEquation(1, lambda x: 2*x, lambda t: 0*t, lambda t: 0*t, [0, 1], [0, 1])
-        M = 10
-        N = 100
-        test_numeric = ForwardDiff(test_eq, M, N)
 
-        w = test_numeric.create_solution_matrix()
-        w_comp = test_numeric.add_boundary_conditions(w)
-        left_bound_val = test_numeric._boundary_condition(test_numeric.eq.l)
-        right_bound_val = test_numeric._boundary_condition(test_numeric.eq.r)
+        w = example_solver.create_solution_matrix()
+        w_comp = example_solver.add_boundary_conditions(w)
+        left_bound_val = example_solver._boundary_condition(example_solver.eq.l)
+        right_bound_val = example_solver._boundary_condition(example_solver.eq.r)
 
         assert w.shape != w_comp.shape
         np.testing.assert_array_equal(w_comp[0,:], left_bound_val)
         np.testing.assert_array_equal(w_comp[-1,:], right_bound_val)
         
 
-    def test_one_step(self):
+    def test_one_step(self, example_solver):
         """Tests that the function one_step updates one step."""
-        test_eq = HeatEquation(1, lambda x: 2*x, lambda t: 0*t, lambda t: 0*t, [0, 1], [0, 1])
-        M = 10
-        N = 100
-        test_numeric = ForwardDiff(test_eq, M, N)
 
-        sol_matrix = test_numeric.create_solution_matrix()
-
-
-        left_side = test_numeric._boundary_condition(test_numeric.eq.l)
-        right_side = test_numeric._boundary_condition(test_numeric.eq.r)
-        v = np.concatenate(([left_side[1]], np.zeros(test_numeric.m - 2), [right_side[1]]))
-        b = test_numeric._sigma() * v
-        A = test_numeric._coefficent_matrix()
+        sol_matrix = example_solver.create_solution_matrix()
+        left_side = example_solver._boundary_condition(example_solver.eq.l)
+        right_side = example_solver._boundary_condition(example_solver.eq.r)
+        v = np.concatenate(([left_side[1]], np.zeros(example_solver.m - 2), [right_side[1]]))
+        b = example_solver._sigma() * v
+        A = example_solver._coefficent_matrix()
         test_matrix = A @ sol_matrix[:, 1] + b
 
-        np.testing.assert_array_equal(test_numeric.one_step(sol_matrix, 1), test_matrix)
+        np.testing.assert_array_equal(example_solver.one_step(sol_matrix, 1), test_matrix)
 
 
-    def test_solve(self):
+    def test_solve(self, example_solver):
         """Tests that the solution matrix is not all zero after solve has been used."""
 
-        test_eq = HeatEquation(1, lambda x: 2*x, lambda t: 0*t, lambda t: 0*t, [0, 1], [0, 1])
-        M = 10
-        N = 100
-        test_numeric = ForwardDiff(test_eq, M, N)
-
-        solution = test_numeric.solve()
+        solution = example_solver.solve()
 
         with np.testing.assert_raises(AssertionError):
-            np.testing.assert_array_equal(solution, np.zeros((M+1, N+1)))
+            np.testing.assert_array_equal(solution, np.zeros((example_solver.m, example_solver.n+1)))
